4fb11d1accd9462c8393ef0662dddecef18878ad
Emk0oo <emir.j2002@gmail.com>
2024-04-04 23:03:20 +0200

    [ADD] first div ok

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customViolet: "#5D47E9",
+      },
+    },
+  },
+  plugins: [],
+};
